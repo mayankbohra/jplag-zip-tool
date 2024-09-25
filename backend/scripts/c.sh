7d2f8daeb2aba5745b0802bb243c6328217005e2
mayankbohra <bohramayank4u@gmail.com>
2024-09-25 19:55:45 +0530

    file updates

--- backend/scripts/c.sh
+++ backend/scripts/c.sh
@@ -1,8 +1,10 @@
-@echo off
+#!/bin/bash
 
-set "JPLAG_JAR=./backend/scripts/jplag.jar"
-set "SUBMISSION_DIR=./backend/uploads"
-set "RESULT_DIR=./backend/results"
-set "LANGUAGE=c"
+# Define variables
+JPLAG_JAR="./backend/scripts/jplag.jar"
+SUBMISSION_DIR="./backend/uploads"
+RESULT_DIR="./backend/results"
+LANGUAGE="c"
 
-java -jar "%JPLAG_JAR%" -l %LANGUAGE% "%SUBMISSION_DIR%" -r "%RESULT_DIR%\result"
+# Run the JPlag command
+java -jar "$JPLAG_JAR" -l "$LANGUAGE" "$SUBMISSION_DIR" -r "$RESULT_DIR/result"
--- backend/scripts/python.sh
+++ backend/scripts/python.sh
@@ -1,8 +1,10 @@
-@echo off
+#!/bin/bash
 
-set "JPLAG_JAR=./backend/scripts/jplag.jar"
-set "SUBMISSION_DIR=./backend/uploads"
-set "RESULT_DIR=./backend/results"
-set "LANGUAGE=python3"
+# Define variables
+JPLAG_JAR="./backend/scripts/jplag.jar"
+SUBMISSION_DIR="./backend/uploads"
+RESULT_DIR="./backend/results"
+LANGUAGE="python3"
 
-java -jar "%JPLAG_JAR%" -l %LANGUAGE% "%SUBMISSION_DIR%" -r "%RESULT_DIR%\result"
+# Run the JPlag command
+java -jar "$JPLAG_JAR" -l "$LANGUAGE" "$SUBMISSION_DIR" -r "$RESULT_DIR/result"
