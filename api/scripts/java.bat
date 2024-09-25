e17247eaea5e3d4353a48d885933b71d0a654baf
mayankbohra <bohramayank4u@gmail.com>
2024-09-25 16:20:58 +0530

    file updates

--- api/scripts/java.bat
+++ api/scripts/java.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set "JPLAG_JAR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\backend\scripts\jplag.jar"
-set "SUBMISSION_DIR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\backend\uploads"
-set "RESULT_DIR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\backend\results"
+set "JPLAG_JAR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\api\scripts\jplag.jar"
+set "SUBMISSION_DIR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\api\uploads"
+set "RESULT_DIR=C:\Users\Mayank\Desktop\college\sem-7\TA\jplag-zip-tool\api\results"
 set "LANGUAGE=java"
 
 java -jar "%JPLAG_JAR%" -l %LANGUAGE% "%SUBMISSION_DIR%" -r "%RESULT_DIR%\result"
