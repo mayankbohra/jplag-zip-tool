8c36280c01ebda680b0d9f5d70d2fc910097a664
mayankbohra <bohramayank4u@gmail.com>
2024-09-25 20:36:43 +0530

    file updates

--- backend/scripts/c.sh
+++ backend/scripts/c.sh
@@ -7,7 +7,7 @@ fi
 
 JPLAG_JAR="./backend/scripts/jplag.jar"
 SUBMISSION_DIR="./backend/uploads"
-RESULT_DIR="./backend/results"
+RESULT_DIR="./backend/results" 
 LANGUAGE="c"
 
 java -jar "$JPLAG_JAR" -l "$LANGUAGE" "$SUBMISSION_DIR" -r "$RESULT_DIR/result"
